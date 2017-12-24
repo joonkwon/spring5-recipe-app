67edec67c1587ad82c84a42f98c9b054d1548a2e
Joon Kwon <Joohyun.Kwon@scotiabank.com>
2017-12-24 13:21:20 -0500

    Recipe Controller modified to add create Recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -3,9 +3,12 @@ package guru.springframework.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 
 @Controller
@@ -23,5 +26,19 @@ public class RecipeController {
 		model.addAttribute("recipe", recipeService.getRecipeById(id) );
 		return "recipe/show";
 	}
+	
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		
+		return "recipe/recipeform";
+	}
 
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+		
+		return "redirect:recipe/show/" + savedCommand.getId();
+	}
 }
